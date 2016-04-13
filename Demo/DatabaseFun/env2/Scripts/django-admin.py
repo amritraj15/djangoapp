f248231efd108d454c160a7452ebd928a7fcaf06
amritraj <amritraj1510@gmail.com>
2016-04-14 00:27:38 +0530

    first commit

---
+++ Demo/DatabaseFun/env2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\amritraj\Documents\Visual Studio 2013\Projects\DatabaseFun\DatabaseFun\env2\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
